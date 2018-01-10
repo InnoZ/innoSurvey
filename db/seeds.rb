98ef10a9bf3b0e4c26daa447349edff0ea32857c
Andi innoz <andneuma@posteo.de>
2018-01-10 17:18:36 +0100

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,40 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+# WIPE EVERYTHING
+Survey.destroy_all
+Topic.destroy_all
+Statement.destroy_all
+Choice.destroy_all
+Role.destroy_all
+
+# CREATE SURVEYS
+2.times do
+  Survey.create(description: Faker::Lorem.sentence(3))
+end
+
+# CREATE ROLES
+Role.create(name: 'Role 1')
+Role.create(name: 'Role 2')
+Role.create(name: 'Role 3')
+
+# CREATE TOPICS
+Survey.all.each do |s|
+  6.times do
+    s.topics.create(description: Faker::Lorem.sentence(3), role: Role.all.sample)
+  end
+end
+
+# CREATE STATEMENTS
+STYLES = %w[multiple single].freeze
+
+Topic.all.each do |t|
+  t.statements.create(style: STYLES.sample, text: Faker::WorldOfWarcraft.quote + '?')
+end
+
+# CREATE CHOICES
+Statement.all.each do |s|
+  (2..5).to_a.sample.times do
+    s.choices.create(text: Faker::WorldOfWarcraft.quote)
+  end
+end
+
