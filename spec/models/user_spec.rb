065ba4b243f71690a79392becd2943666dddf22c
Andi innoz <andneuma@posteo.de>
2018-01-16 12:23:36 +0100

    Remove enabled attribute from user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 describe User do
   context 'Attributes' do
     it { is_expected.to respond_to(:email) }
-    it { is_expected.to respond_to(:enabled) }
     it { is_expected.to respond_to(:password) }
     it { is_expected.to respond_to(:password_confirmation) }
   end
