a5e0bbf3ce8e5115ba1caeadc88fbefa964f1361
Andi innoz <andneuma@posteo.de>
2018-01-17 17:16:22 +0100

    Can commit answers to DB

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,21 @@
 class AnswersController < ApplicationController
-  skip_before_action :verify_authenticity_token
+  def answer_question_set
+    respond_to do |format|
+      format.json do
+        answers.each do |answer|
+          Answer.create(statement_id: answer["statement_id"], result: answer["selected_choices"].to_s)
+        end
+      end
+    end
+  end
 
-  def mock
-    p 'POST REQUEST ARRIVED: '
-    p params
+  private
+
+  def results_to_hash
+    JSON.parse params["answers"]
+  end
+
+  def answers
+    results_to_hash["answers"]
   end
 end
