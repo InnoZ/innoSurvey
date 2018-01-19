9ea25af96063d35d954164c5736248bf62bfb637
oiejf873l <oiejf873l@systemli.org>
2018-01-19 12:39:12 +0100

    Answer: Recover selected choices validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,12 +1,37 @@
 class Answer < ApplicationRecord
   belongs_to(:statement, dependent: :destroy)
 
+  validate :selected_choices_references
+
   def selected_choices_inst
-    selected_choices_ids = selected_choices.match(/\[(.*)\]/)[1].split(',')
     Choice.where(id: selected_choices_ids)
   end
 
   def selected_choices_ids
     selected_choices.match(/\[(.*)\]/)[1].split(',').map(&:to_i)
+  end
+
+  private
+
+  def selected_choices_references
+    has_choice_reference = selected_choices_ids.map { |id| Choice.where(id: id).any? }
+
+    # Check that at least on reference
+    if selected_choices_ids.empty?
+      errors.add(:selected_choices, 'Auswahl muss mindestens ein Element enthalten!')
+    end
+
+    # Check for non-existing choices
+    if has_choice_reference.include? false
+      errors.add(:selected_choices, 'Auswahl enthält ungültige Referenzen!')
+    end
+    
+    # Check that all choices refer to same statement
+    if selected_choices_inst.pluck(:statement_id).uniq.count > 1
+      errors.add(:selected_choices, 'Auswahl enthält Referenzen auf unterschiedliche Statements!')
+      return nil
+    end
+
+    return false
   end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -18,4 +18,53 @@ RSpec.describe Answer, type: :model do
       expect(answer.selected_choices_inst).to eq choices
     end
   end
+
+  context 'Validations' do
+    before do
+      create_choice_stack
+      choices0_ids = @statements[0].choices.pluck(:id)
+      choices1_ids = @statements[1].choices.pluck(:id)
+      choices2_ids = @statements[2].choices.pluck(:id)
+
+      @answers_hash = { answers: [
+        { statement_id: @statements[0].id, selected_choices: choices0_ids.slice(0, 1) },
+        { statement_id: @statements[1].id, selected_choices: choices1_ids.slice(1, 1) },
+        { statement_id: @statements[2].id, selected_choices: choices2_ids.slice(1, 2) }, ]
+      }
+      @answer = @answers_hash[:answers].first
+    end
+
+    it 'can save valid answer' do
+      expect { Answer.create(@answer) }.to change(Answer, :count).by(1)
+    end
+
+    context 'invalid answers' do
+      it 'will not save answer with selected choices referring to non-existing statements' do
+        @answer[:selected_choices] = [9999]
+        new_answer = Answer.new(@answer)
+
+        expect(new_answer).not_to be_valid
+      end
+
+      it 'will not save answer with selected choices referring to statements from different statements' do
+        another_statement = create :statement, statement_set: @statement_set
+        choice_for_another_statement = create :choice, statement: another_statement
+
+        @answer[:selected_choices] << choice_for_another_statement.id
+        new_answer = Answer.new(@answer)
+
+        expect(new_answer).not_to be_valid
+      end
+
+      it 'will not save answer with no selected choices' do
+        another_statement = create :statement, statement_set: @statement_set
+        choice_for_another_statement = create :choice, statement: another_statement
+
+        @answer[:selected_choices] = []
+        new_answer = Answer.new(@answer)
+
+        expect(new_answer).not_to be_valid
+      end
+    end
+  end
 end
