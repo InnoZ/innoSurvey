85c298d06c1909d32d60c5a9803b2323f7cebd91
kjoscha <kjoscha@gmail.com>
2018-03-09 10:53:51 +0100

    move main components in highest pack level file

--- app/javascript/packs/exhibitionScreen.jsx
+++ app/javascript/packs/exhibitionScreen.jsx
@@ -4,7 +4,71 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ExhibitionScreen from './components/exhibitionScreen.jsx';
+import Scanner from './components/scanner.jsx';
+import StatementSet from './components/statementSet.jsx';
+
+class ExhibitionScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      scan: true,
+      roleId: null,
+      uuid: null,
+    }
+  }
+
+  ident(identString) {
+    if (identString.includes('uuid') && identString.includes('role_id')) {
+      let identJson = JSON.parse(identString);
+      this.setState({
+        scan: false,
+        roleId: identJson['role_id'],
+        uuid: identJson['uuid'],
+      })
+    };
+  };
+
+  reset() {
+    this.setState({
+      scan: true,
+      roleId: null,
+      uuid: null,
+    })
+  }
+
+  statementSetFromRole(roleId) {
+    return window.data.statement_sets.find((set) =>
+      set.role_id == this.state.roleId
+    );
+  }
+
+  render() {
+    let main;
+    if (this.state.scan == true) {
+      main = <Scanner ident={this.ident.bind(this)} />
+    } else {
+      const set = this.statementSetFromRole(this.state.roleId);
+      if (set) {
+        main = <StatementSet
+          roleId={set.role_id}
+          uuid={this.state.uuid}
+          roleName={set.role_name}
+          statements={set.statements}
+          reset={() => this.reset()}
+        />
+      } else {
+        main = <div>
+          <h3>Mit deinem QR-Code kann ich nichts anfangen :(</h3>
+          <div onClick={() => this.reset()}>Klicke hier um zum Scanner zurück zu gelangen</div>
+        </div>
+      }
+    }
+
+    return (
+      <div>{main}</div>
+    );
+  }
+}
 
 ReactDOM.render(
   <div className='all-centered'>
