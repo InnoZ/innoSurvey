c2907d0d3da33b6023b3dfc828f4af9f03002a49
Paul K <paul.kitt@innoz.de>
2018-03-14 14:44:06 +0100

    Added enpoint replying which topics a user has finished

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,4 +7,19 @@ class TopicsController < ApplicationController
       format.json { render json: @topic_json.to_json, status: 200 }
     end
   end
+  # Ajax endpoint which returns which topics a given user(with specific role)
+  # has allready fully answered
+  def answered_topics_by_user
+    # Get all statements
+    statements = Statement.all.index_by(&:id)
+    statement_sets = StatementSet.all.index_by(&:id)
+    # Get all answers for a given user id
+    user_answers = Answer.where(uuid: params[:id])
+    # Iterate answers and 
+    topic_ids = user_answers.map do |a| 
+      statement_sets[statements[a.statement_id].statement_set_id].topic_id
+    end
+    render json: topic_ids.uniq, status:200
+  end
 end
+
