2f9ea9e398a59609651b7d59fe20ff06a5448bb6
kjoscha <kjoscha@gmail.com>
2018-05-25 16:06:33 +0200

    answers view

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,6 +2,7 @@ class Survey < ApplicationRecord
   has_many(:stations, dependent: :destroy)
   has_many(:roles, dependent: :destroy)
   has_many(:topics, through: :stations)
+  has_many(:answers, through: :topics)
   belongs_to(:user)
 
   validates :description, presence: true, length: {  minimum: 10 }
