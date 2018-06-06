0c415bba362a3392a0f9ab537f2b40c91f02ee29
kjoscha <kjoscha@gmail.com>
2018-06-06 17:10:33 +0200

    safer key

--- lib/encrypt_decrypt.rb
+++ lib/encrypt_decrypt.rb
@@ -4,16 +4,16 @@
 require 'openssl'
 
 class String
-  def encrypt(key: 'FhMdavn9o7FZwTnmvJ8BCqsov89miw6b8LeikQvBjMpjEjzDitfa87zvZiraffHf')
-    cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').encrypt
-    cipher.key = key
+  def encrypt(key: 'ratzEncryptTheInnoZ')
+    cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').encrypt
+    cipher.key = Digest::SHA512.hexdigest key
     s = cipher.update(self) + cipher.final
     s.unpack('H*')[0].upcase
   end
 
-  def decrypt(key: 'FhMdavn9o7FZwTnmvJ8BCqsov89miw6b8LeikQvBjMpjEjzDitfa87zvZiraffHf')
-    cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').decrypt
-    cipher.key = key
+  def decrypt(key: 'ratzEncryptTheInnoZ')
+    cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').decrypt
+    cipher.key = Digest::SHA512.hexdigest key
     s = [self].pack("H*").unpack("C*").pack("c*")
     cipher.update(s) + cipher.final
   end
