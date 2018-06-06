d8fc412b25954112e89da22c4cd09af30ea14a56
kjoscha <kjoscha@gmail.com>
2018-06-06 16:36:01 +0200

    extend encryption key to 24bytes

--- lib/encrypt_decrypt.rb
+++ lib/encrypt_decrypt.rb
@@ -4,14 +4,14 @@
 require 'openssl'
 
 class String
-  def encrypt(key: 'ratzEncryptTheInnoZ')
+  def encrypt(key: 'ratzEncryptTheInnoZin2018')
     cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').encrypt
     cipher.key = Digest::SHA1.hexdigest key
     s = cipher.update(self) + cipher.final
     s.unpack('H*')[0].upcase
   end
 
-  def decrypt(key: 'ratzEncryptTheInnoZ')
+  def decrypt(key: 'ratzEncryptTheInnoZin2018')
     cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').decrypt
     cipher.key = Digest::SHA1.hexdigest key
     s = [self].pack("H*").unpack("C*").pack("c*")
