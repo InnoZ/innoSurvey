95c1fe39a3e6808cbbb08955a4755190da0b5ffd
kjoscha <kjoscha@gmail.com>
2018-06-06 16:20:19 +0200

    add encrypted token to assure token athenticity

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,12 @@
 class AnswersController < ApplicationController
+  require 'encrypt_decrypt'
+
   skip_before_action :verify_authenticity_token
 
   def answer_question_set
     respond_to do |format|
       format.json do
-        unless contains_invalid_answer?
+        unless contains_invalid_answer? || !token_valid
           answers.each { |answer| Answer.create(answer.merge!(uuid: uuid)) }
           render(json: nil, status: :created)
         else
@@ -16,8 +18,20 @@ class AnswersController < ApplicationController
 
   private
 
+  def token_valid
+    token.decrypt == uuid || (belongs_to_old_survey && token.decrypt == 'token4oldSurveys')
+  end
+
+  def belongs_to_old_survey
+    # old survey does not have token in their qr codes
+    statement_id = Statement.find(answers.first[:statement_id]).survey.id
+    [5, 4].include?(statement_id)
+  end
+
   def contains_invalid_answer?
-    validations = answers.map { |answer| valid_answer?(answer)  }
+    validations = answers.map do |answer|
+      valid_answer?(answer)
+    end
     validations.include? false
   end
 
@@ -32,10 +46,14 @@ class AnswersController < ApplicationController
   end
 
   def answers
-    params.permit(:uuid, answers: [ :statement_id, selected_choices: [] ])[:answers]
+    params.permit(:uuid, :token, answers: [ :statement_id, selected_choices: [] ])[:answers]
   end
 
   def uuid
     params.permit(:uuid)[:uuid]
+  end
+
+  def token
+    params.permit(:token)[:token]
   end
 end
--- spec/features/exhibition_view_spec.rb
+++ spec/features/exhibition_view_spec.rb
@@ -1,3 +1,5 @@
+require 'encrypt_decrypt'
+
 feature 'Exhibition view', :js do
   before do
     @survey = create(:survey)
@@ -20,8 +22,21 @@ feature 'Exhibition view', :js do
     select_other_question_and_see_related_answers
     select_and_highlight_one_choice
     send_question_set_and_see_qr_scan_view
-    scan_same_code_again_and_get_error_message
+
+    select_role_via_qr_code_scan
+    get_error_message_because_same_code
     scan_code_with_invalid_role_id_and_jump_back_to_initial_screen
+  end
+
+  scenario 'does not work without correct token within qr code' do
+    visit topic_ident_path(@topic.id)
+
+    scan_wrong_token
+    select_and_highlight_one_choice
+    do_not_show_send_button_unless_all_statements_answered
+    select_other_question_and_see_related_answers
+    select_and_highlight_one_choice
+    send_question_set_and_do_not_save
   end
 
   scenario 'shows Scanner immediately and redirects to url if url is transmitted' do
@@ -31,15 +46,19 @@ feature 'Exhibition view', :js do
     expect(page).to have_content('Station: Sample station')
   end
 
-  def scan_same_code_again_and_get_error_message
-    select_role_via_qr_code_scan
+  def get_error_message_because_same_code
     expect(page).to have_content('Hier warst du schon')
     find('.question').trigger('click')
   end
 
   def select_role_via_qr_code_scan
     find('.question', text: 'Klicke hier, um die Umfrage zu starten').trigger('click')
-    scan(uuid: '123xy', role_id: '1')
+    scan(uuid: '123xy', role_id: '1', token: '123xy'.encrypt)
+  end
+
+  def scan_wrong_token
+    find('.question', text: 'Klicke hier, um die Umfrage zu starten').trigger('click')
+    scan(uuid: '123xy', role_id: '1', token: 'wrong'.encrypt)
   end
 
   def do_not_show_send_button_unless_all_statements_answered
@@ -68,17 +87,33 @@ feature 'Exhibition view', :js do
     expect(page).to have_content('Klicke hier, um die Umfrage zu starten')
   end
 
+  def send_question_set_and_do_not_save
+    click_on('Absenden')
+    sleep 1
+    expect(page).to_not have_content('gespeichert')
+  end
+
   def scan_code_with_invalid_role_id_and_jump_back_to_initial_screen
-    scan(uuid: '123xy', role_id: '666')
+    scan(uuid: '123xy', role_id: '666', token: '123xy'.encrypt)
     expect(page).to have_content('Dein QR-Code passt nicht zu dieser Umfrage')
     expect(page).to have_content('Klicke hier, um die Umfrage zu starten')
   end
 
-  def scan(uuid:, role_id:)
+  def scan_code_with_wrong_token_and_do_not_save_answers
+    find('.question', text: 'Klicke hier, um die Umfrage zu starten').trigger('click')
+    scan(uuid: '123xy', role_id: '1', token: 'wrong'.encrypt)
+    find('.next-button').trigger('click')
+    click_on('Absenden')
+    expect(page).to have_content('gespeichert')
+    expect(page).to have_content('Klicke hier, um die Umfrage zu starten')
+  end
+
+  def scan(uuid:, role_id:, token:)
     # since qr scan cannot be tested, it is faked by hidden form
     # see related function in scanner react component
     find('input#uuid-test-input', visible: false).set(uuid)
     find('input#role-id-test-input', visible: false).set(role_id)
+    find('input#token-test-input', visible: false).set(token)
     find('#send-fake-qr', visible: false).trigger('click')
   end
 end
