44f02deaa49595780ce49d9079213505791ea404
kjoscha <kjoscha@gmail.com>
2018-06-06 17:24:48 +0200

    try with another encryption method

--- lib/encrypt_decrypt.rb
+++ lib/encrypt_decrypt.rb
@@ -5,14 +5,14 @@ require 'openssl'
 
 class String
   def encrypt(key: 'ratzEncryptTheInnoZ')
-    cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').encrypt
+    cipher = OpenSSL::Cipher.new('AES-256-CBC').encrypt
     cipher.key = Digest::SHA512.hexdigest key
     s = cipher.update(self) + cipher.final
     s.unpack('H*')[0].upcase
   end
 
   def decrypt(key: 'ratzEncryptTheInnoZ')
-    cipher = OpenSSL::Cipher::Cipher.new('DES-EDE3-CBC').decrypt
+    cipher = OpenSSL::Cipher.new('AES-256-CBC').decrypt
     cipher.key = Digest::SHA512.hexdigest key
     s = [self].pack("H*").unpack("C*").pack("c*")
     cipher.update(s) + cipher.final
