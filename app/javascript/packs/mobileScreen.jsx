f62bc1a4443ddb7f1368a0dcfe6ecf2adb3a8059
Paul Kitt <paul.kitt@innoz.de>
2018-11-19 15:46:30 +0100

    Add new react entrypoint for existing uuid/token, roleId. topicId/stationId

--- app/javascript/packs/mobileScreen.jsx
+++ app/javascript/packs/mobileScreen.jsx
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Scanner from './components/scanner.jsx';
 import TopicSelection from './components/topicSelection.jsx';
+import StatementSet from './components/statementSet.jsx';
 
 class MobileScreen extends React.Component {
   constructor(props) {
@@ -19,6 +20,7 @@ class MobileScreen extends React.Component {
   }
 
   componentWillMount() {
+    //Check if neccesary parameters are given to prevent qr code reading
     if (getUrlParam(window.location.href, 'uuid') && getUrlParam(window.location.href, 'role_id') && getUrlParam(window.location.href, 'token')) {
       const newState = {
         scan: false,
@@ -29,6 +31,16 @@ class MobileScreen extends React.Component {
       console.log('uuid: ' + newState.uuid)
       console.log('role id: ' + newState.roleId)
       console.log('token: ' + newState.token)
+      this.setState(newState);
+
+      //Check if a station is allready given otherwise station overview is presented
+      if (getUrlParam(window.location.href, 'topic_id') && getUrlParam(window.location.href, 'station_id')) { 
+        this.setState({
+          activeStation: getUrlParam(window.location.href, 'station_id'),
+          activeTopic: getUrlParam(window.location.href, 'topic_id')
+        });
+      }
+
       this.setState(newState);
     }
   }
@@ -43,10 +55,36 @@ class MobileScreen extends React.Component {
     this.setState(newState)
   };
 
+  topic() {
+    const station = window.stations.find((station) => station.id == this.state.activeStation)
+    return station.topics.find((topic) => topic.id == this.state.activeTopic)
+  }
+
+  statementSet() {
+    return this.topic().statement_sets.find((set) => set.role_id == this.state.roleId)
+  }
+  reset(){
+   console.log("schmu");
+  }
+
   render() {
     let main;
     if (this.state.scan) {
       main = <Scanner ident={this.ident.bind(this)} />
+    } else if (this.state.activeTopic) {
+      const statementSet = this.statementSet();
+      main = statementSet
+        ? <StatementSet topicId={this.state.activeTopic}
+                        roleId={this.state.roleId}
+                        uuid={this.state.uuid}
+                        token={this.state.token}
+                        roleName={statementSet.role_name}
+                        statements={statementSet.statements}
+                        reset={() => this.reset()} />
+        : <div className='question' onClick={this.reset.bind(this)}>
+            Hier gibt es leider keine Fragen für deine Rolle.
+            <div className='subtitle'>Klicke hier um zurück zu gelangen.</div>
+          </div>
     } else {
       main = <TopicSelection roleId={this.state.roleId} uuid={this.state.uuid} token={this.state.token} />
     };
