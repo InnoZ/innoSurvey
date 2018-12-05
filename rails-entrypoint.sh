4e7cb145c4396864952e7cd28ca2a44f0d482140
Paul Kitt <paul.kitt@innoz.de>
2018-12-05 01:58:03 +0100

    Fix entrypoint;Add docker prod

--- rails-entrypoint.sh
+++ rails-entrypoint.sh
@@ -2,7 +2,7 @@
 bundle exec rails assets:clobber
 bundle exec rails assets:precompile
 
-rake db:exists && rake db:migrate || rake db:setup
+bundle exec rake db:exists && bundle exec rake db:migrate || bundle exec rake db:setup
 
 rm -rf ./tmp/pids/server.pid
 bundle exec rails server
